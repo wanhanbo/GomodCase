751b50c86f5c634ada85e96896a114121001d565
wanhanbo <hbowan@163.com>
2023-12-24 17:09:36 +0800

    fix the unstable bug for v4.0.0-pre

--- utils/ping.go
+++ utils/ping.go
@@ -13,14 +13,14 @@ func Say(a string, b string) {
 func SayOne(a float64) {
 	fmt.Println("===go mod test case===")
 	fmt.Printf("you only say (double):%v\n", a)
-	//panic("there is a bug:\n ")  //the bug has been fixed
+	//panic("there is a bug:\n ")  //the bug has been fixed after v3.0.0
 }
 
 func SayRandomly() {
 	content := rand.Intn(100)
 	fmt.Println(content)
-	if content < 5 {
-		panic("an error happens because of the unstable version")
-	}
+	//if content < 5 {
+	//	panic("an error happens because of the unstable version")
+	//} //the unstable bug has been fixed after v4.0.0-pre
 
 }
