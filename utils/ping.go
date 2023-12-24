99d9a1b33693785f54e8b955af7162f21e3ecf22
wanhanbo <hbowan@163.com>
2023-12-24 16:25:17 +0800

    Backwards compatible releases

--- utils/ping.go
+++ utils/ping.go
@@ -6,3 +6,8 @@ func Say(a string, b string) {
 	fmt.Println("===go mod test case===")
 	fmt.Printf("you say:%s and %s", a, b)
 }
+
+func SayOne(a string) {
+	fmt.Println("===go mod test case===")
+	fmt.Printf("you only say:%s ", a)
+}
