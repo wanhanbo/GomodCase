a8e4ee67bc01d72a0c591931f85a725928a2d576
wanhanbo <hbowan@163.com>
2023-12-24 17:00:47 +0800

    a unstable version for v4.0.0

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "github.com/wanhanbo/GomodCase/v3/utils"
+import "github.com/wanhanbo/GomodCase/v4/utils"
 
 func main() {
 	utils.SayOne(float64(2))
+	for i := 0; i < 10; i++ {
+		utils.SayRandomly()
+	}
 
 }
--- utils/ping.go
+++ utils/ping.go
@@ -1,6 +1,9 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func Say(a string, b string) {
 	fmt.Println("===go mod test case===")
@@ -11,4 +14,13 @@ func SayOne(a float64) {
 	fmt.Println("===go mod test case===")
 	fmt.Printf("you only say (double):%v\n", a)
 	//panic("there is a bug:\n ")  //the bug has been fixed
+}
+
+func SayRandomly() {
+	content := rand.Intn(100)
+	fmt.Println(content)
+	if content < 5 {
+		panic("an error happens because of the unstable version")
+	}
+
 }
